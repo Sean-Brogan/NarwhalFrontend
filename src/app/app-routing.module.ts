261218288b9f82c91b7af23a166bba8e199fafb5
jessestell <jstell94@gmail.com>
2017-10-16 20:03:09 -0500

    This is my baby now, baby, baby, baby.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,35 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent }     from './login/login.component';
 import { AuthGuard }          from './authentication/authentication.guard';
+import { PatientsComponent } from "./patients/patients.component";
 
-const routes: Routes = [
-  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'login',  component: LoginComponent },
 
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'patients',
+    component: PatientsComponent
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AuthGuard]
+  },
   // otherwise redirect to the dashboard
   { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule {}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,7 +5,19 @@ import { UserService } from '../user/user.service';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'dashboard.component.html'
+    template:`
+      <div class="col-md-6 col-md-offset-3">
+        <nav>
+          <a routerLink="/dashboard" routerLinkActive="active">Records</a>
+          <a routerLink="/patients" routerLinkActive="active">Patients</a>
+          <a [routerLink]="['/login']">Logout</a>
+        </nav>
+        <h1>Hi {{currentUser.firstName}}!</h1>
+        <p>Welcome to the <i>Narwhal Project</i></p>
+        <p><a [routerLink]="['/login']">Logout</a></p>
+      </div>
+      <router-outlet></router-outlet>
+    `
 })
 
 export class DashboardComponent implements OnInit {
