6bfab5a771b9e8f9089eb91057d95c88c7a154e2
SeanBrogan <broganpsean@gmail.com>
2017-10-23 16:52:12 -0500

    some random shit

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -9,14 +9,9 @@ export class AuthenticationService {
     constructor(private http: Http) { }
 
     login(username: string, password: string) {
-      let cpHeaders = new Headers({ 'Context-Type': 'application/json' });
-      let myParams: URLSearchParams = new URLSearchParams();
-      myParams.set('username', JSON.stringify(username));
-      myParams.set('password', JSON.stringify(password));
-      let options = new RequestOptions({ search: myParams });
         return this.http.get('http://localhost:8080/login?username=' + username + '&password=' + password )
-            .map(_body => {
-                let user = _body.json();
+            .map(response => {
+                let user = response.json();
                 if (user) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
