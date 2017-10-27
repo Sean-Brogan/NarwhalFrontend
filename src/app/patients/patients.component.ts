a43ac154f428bae4a8541abaf8f26ac046d881f4
SeanBrogan <broganpsean@gmail.com>
2017-10-26 20:12:24 -0500

    commit to pull

--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -4,27 +4,7 @@ import { User } from '../models/user';
 import { UserService } from '../user/user.service';
 
 @Component({
-  template:`
-    <div class="body">
-      <div class="head">
-        <a class="logo" routerLink="/dashboard" routerLinkActive="active">The Narwhal Project</a>
-      </div>
-      <div class="nav">
-        <a class="nav" routerLink="/dashboard" routerLinkActive="active">Records</a>
-        <a class="current" routerLink="/patients" routerLinkActive="active">Patients</a>
-        <a class="nav" routerLink="/accounts" routerLinkActive="active">Account</a>
-        <a class="nav" [routerLink]="['/login']">Logout</a>
-      </div>
-      <div class="info">
-      <h1>Hi {{currentUser.firstName}}!</h1>
-      <p>Let's do some shit, friends.</p>
-      <a href="#">Add a Record</a><br>
-      <a href="#">Don't Add A Record</a>
-      <p><a [routerLink]="['/login']">Logout</a></p>
-    </div>
-    </div>
-    <router-outlet></router-outlet>
-  `
+  templateUrl:'patients.component.html'
 })
 export class PatientsComponent implements OnInit {
   currentUser: User;
